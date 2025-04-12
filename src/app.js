4d96676adfaacd91c0e394d55bef2be42a805410
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2025-04-12 11:20:28 -0400

    add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express  from "express";
+import cors from "cors"
 
 import contactRouter from "./routes/contact.routes.js"
 
@@ -6,6 +7,29 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+const allowedOrigins = [
+    'https://portafolio-pablo-arce.vercel.app',
+    "http://localhost:8080"
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    credentials: true
+};
+
+
+
+app.use(cors(corsOptions));
 app.use("/api", contactRouter)
 
 export default app
